46e2f01afe4982d2e5f7a33bba731fde82d20837
Administrator <Administrator@DESKTOP-CFFSQ6I>
2023-04-07 08:23:23 +0800

    1

--- AutoEmailSender/Program.cs
+++ AutoEmailSender/Program.cs
@@ -5,37 +5,48 @@ using System.Net.Mail;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AutoEmailSender
 {
     internal class Program
     {
-        static string networkSharePath = ConfigurationManager.AppSettings["NetworkSharePath"];
-        static string fileExtension = ConfigurationManager.AppSettings["FileExtension"];
-        static string fileNamePrefix = ConfigurationManager.AppSettings["FileNamePrefix"];
-        static string fromEmail = ConfigurationManager.AppSettings["FromEmail"];
-        static string fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassword"];
-        static string toEmail = ConfigurationManager.AppSettings["ToEmail"];
-        static string reminderToEmail = ConfigurationManager.AppSettings["ReminderToEmail"];
-        static string smtpServer = ConfigurationManager.AppSettings["SmtpServer"];
-        static int smtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"]);
-        static int startTime = Convert.ToInt32(ConfigurationManager.AppSettings["StartTime"]);
-        static bool enableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
+        static readonly string networkSharePath = ConfigurationManager.AppSettings["NetworkSharePath"];
+        static readonly string fileExtension = ConfigurationManager.AppSettings["FileExtension"];
+        static readonly string fileNamePrefix = ConfigurationManager.AppSettings["FileNamePrefix"];
+        static readonly string fromEmail = ConfigurationManager.AppSettings["FromEmail"];
+        static readonly string fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassword"];
+        static readonly string toEmail = ConfigurationManager.AppSettings["ToEmail"];
+        static readonly string reminderToEmail = ConfigurationManager.AppSettings["ReminderToEmail"];
+        static readonly string smtpServer = ConfigurationManager.AppSettings["SmtpServer"];
+        static readonly int smtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"]);
+        static readonly bool enableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
         static bool enbaleSend = false;
-        static void Main(string[] args)
-       
+
+        static async Task Main(string[] args)
         {
-            
-            TimeSpan timeUntilNext14pm = GetNextOccurrence(new TimeSpan(14, 0, 0));
-            TimeSpan timeUntilNext16pm = GetNextOccurrence(new TimeSpan(16, 0, 0));
-            SetTimerForNextTask(timeUntilNext14pm, TaskAt14pm);
-            SetTimerForNextTask(timeUntilNext16pm, TaskAt16pm);
-     
-            Console.WriteLine("设置多个任务的启动时间完毕");
-            Console.WriteLine("按Enter键结束...");
-            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "FMD邮件自动推送";
+            var task1 = ScheduleTask(new TimeSpan(14, 10, 0), TaskAt14pm, "TaskAt14pm");
+            var task2 = ScheduleTask(new TimeSpan(16, 0, 0), TaskAt16pm, "TaskAt16pm");
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}  FMD邮件自动发送任务已启用");
+            await Task.WhenAll(task1, task2);
         }
-        
+
+        private static async Task ScheduleTask(TimeSpan targetTime, Action task, string taskName)
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                DateTime todayTargetTime = DateTime.Today.Add(targetTime);
+                DateTime nextOccurrence = todayTargetTime > now ? todayTargetTime : todayTargetTime.AddDays(1);
+                TimeSpan timeUntilNextTask = nextOccurrence - now;
+                await Task.Delay(timeUntilNextTask);
+                task();
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} Task executed: {taskName} 已执行");
+            }
+        }
+
         private static void TaskAt14pm()
         {
 
@@ -44,7 +55,8 @@ namespace AutoEmailSender
 
             if (dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday)
             {
-                Console.WriteLine("今天是周六或周日，不会发送邮件。");
+                
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 今天是周六或周日，不会发送邮件");
             }
             else
             {
@@ -52,17 +64,14 @@ namespace AutoEmailSender
                 if (filesToSend.Count != 0)
                 {
                     SendEmailWithAttachments(fromEmail, fromEmailPassword, toEmail, smtpServer, smtpPort, enableSsl, filesToSend);
-                    Console.WriteLine("已执行14:00 PM邮件推送任务。当前时间：{0:HH:mm:ss}", DateTime.Now);
                     enbaleSend = true; 
-
                 }
                 else
                 {
-                    SendReminderEmail(fromEmail, fromEmailPassword, reminderToEmail, smtpServer, smtpPort, enableSsl, startTime);
-                    Console.WriteLine("没有找到文件。当前时间：{0:HH:mm:ss}", DateTime.Now);
+                    SendReminderEmail(fromEmail, fromEmailPassword, reminderToEmail, smtpServer, smtpPort, enableSsl);
+                    
                 }
-            }
-            
+            }            
         }
 
         private static void TaskAt16pm()
@@ -73,55 +82,35 @@ namespace AutoEmailSender
 
             if (dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday)
             {
-                Console.WriteLine("今天是周六或周日，不会发送邮件。");
+                
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 今天是周六或周日，不会发送邮件");
             }
+            
             else
             {
                 var filesToSend = GetFilesFromNetworkShare(networkSharePath, fileExtension, fileNamePrefix);
                 if (filesToSend.Count != 0 && enbaleSend == false)
                 {
-                    SendEmailWithAttachments(fromEmail, fromEmailPassword, toEmail, smtpServer, smtpPort, enableSsl, filesToSend);
-                    Console.WriteLine("已执行16:00 PM邮件推送任务。当前时间：{0:HH:mm:ss}", DateTime.Now);
+                    SendEmailWithAttachments(fromEmail, fromEmailPassword, toEmail, smtpServer, smtpPort, enableSsl, filesToSend);                  
 
                 }
                 else if (filesToSend.Count != 0 && enbaleSend == true)
                 {
-                    Console.WriteLine("已执行过14:00 PM邮件推送任务，无需重复发送。当前时间：{0:HH:mm:ss}", DateTime.Now);
+                    Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 已执行过14:00 邮件推送任务，无需重复发送");
                     enbaleSend = false;
                 }
                 else
                 {
-                    Console.WriteLine("没有找到文件。当前时间：{0:HH:mm:ss}", DateTime.Now);
+                    // 保存原始控制台颜色
+                    ConsoleColor originalColor = Console.ForegroundColor;
+                    // 将颜色更改为黄色并打印这一行
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 没有找到文件");
+                    // 将控制台颜色重置为原始颜色
+                    Console.ForegroundColor = originalColor;
                 }
             }
             
-        }
-        
-        private static void SetTimerForNextTask(TimeSpan timeUntilNextTask, Action task)
-        {
-            // 创建定时器，并设置触发时间
-            Timer timer = new Timer(OnTimedEvent, task, timeUntilNextTask, Timeout.InfiniteTimeSpan);
-        }
-
-        private static void OnTimedEvent(object state)
-        {
-            // 获取要执行的任务
-            Action task = (Action)state;
-
-            // 执行任务
-            task();
-
-            // 重新设置定时器以在明天的相同时间执行任务
-            TimeSpan timeUntilNextOccurrence = GetNextOccurrence(TimeSpan.FromDays(1));
-            SetTimerForNextTask(timeUntilNextOccurrence, task);
-        }
-
-        private static TimeSpan GetNextOccurrence(TimeSpan targetTime)
-        {
-            DateTime now = DateTime.Now;
-            DateTime todayTargetTime = DateTime.Today.Add(targetTime);
-            DateTime nextOccurrence = todayTargetTime > now ? todayTargetTime : todayTargetTime.AddDays(1);//如果已经过了指定时间，就等于明天定时的小时时间
-            return nextOccurrence - now;
         }
         
         private static List<string> GetFilesFromNetworkShare(string networkSharePath, string fileExtension, string fileNamePrefix)
@@ -129,7 +118,7 @@ namespace AutoEmailSender
             List<string> filesToSend = new List<string>();
             try
             {
-                Console.WriteLine($"正在从网络共享路径 '{networkSharePath}' 获取以'{fileNamePrefix}'开头且创建于当天的{fileExtension}文件...");
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 正在从网络共享路径 '{networkSharePath}' 获取以'{fileNamePrefix}'开头且创建于当天的{fileExtension}文件...");
                 string[] files = Directory.GetFiles(networkSharePath, "*" + fileExtension);
                 DateTime today = DateTime.Today;
 
@@ -142,13 +131,12 @@ namespace AutoEmailSender
                         filesToSend.Add(file);
                     }
                 }
-                Console.WriteLine($"找到 {filesToSend.Count} 个符合条件的{fileExtension}文件:");
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 找到 {filesToSend.Count} 个符合条件的{fileExtension}文件");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"获取文件时出错: {ex.Message}");
             }
-
             return filesToSend;
         }
 
@@ -156,14 +144,14 @@ namespace AutoEmailSender
         {
             if (filesToSend.Count == 0)
             {
-                Console.WriteLine("没有找到符合条件的文件，不会发送邮件。");
-                return;
-            }
-            DateTime today = DateTime.Today;
-            DayOfWeek dayOfWeek = today.DayOfWeek;
-            if (dayOfWeek == DayOfWeek.Sunday)
-            {
-                Console.WriteLine("今天是周六或周日，不会发送邮件。");
+                // 保存原始控制台颜色
+                ConsoleColor originalColor = Console.ForegroundColor;
+
+                // 将颜色更改为黄色并打印这一行
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} 没有找到符合条件的文件，不会发送邮件");
+                // 将控制台颜色重置为原始颜色
+                Console.ForegroundColor = originalColor;
                 return;
             }
             try
@@ -184,7 +172,7 @@ namespace AutoEmailSender
                         smtp.Credentials = new NetworkCredential(fromEmail, fromEmailPassword);
                         smtp.EnableSsl = enableSsl; // 如果需要，请启用SSL
                         smtp.Send(mail);
-                        Console.WriteLine("邮件已成功发送！");
+                        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} FMD邮件已成功发送！");
                     }
                 }
             }
@@ -194,7 +182,7 @@ namespace AutoEmailSender
             }
         }
         
-        private static void SendReminderEmail(string fromEmail, string fromEmailPassword, string toEmail, string smtpServer, int smtpPort, bool enableSsl, int startTime)
+        private static void SendReminderEmail(string fromEmail, string fromEmailPassword, string toEmail, string smtpServer, int smtpPort, bool enableSsl)
         {
             try
             {
@@ -203,14 +191,19 @@ namespace AutoEmailSender
                     mail.From = new MailAddress(fromEmail);
                     mail.To.Add(toEmail);
                     mail.Subject = "FMD提醒";
-                    mail.Body = "今天下午" + startTime.ToString() + "点没有找到符合条件的FMD文件，请注意。";
+                    mail.Body = $"今天下午{DateTime.Now:yyyy-MM-dd HH:mm:ss} 没有找到符合条件的FMD文件，请注意";
                     using (SmtpClient smtp = new SmtpClient(smtpServer, smtpPort)) // 请根据你的邮箱服务提供商设置SMTP服务器地址和端口
                     {
                         smtp.Credentials = new NetworkCredential(fromEmail, fromEmailPassword);
                         smtp.EnableSsl = enableSsl; // 如果需要，请启用SSL
                         smtp.Send(mail);
+                        // 保存原始控制台颜色
+                        ConsoleColor originalColor = Console.ForegroundColor;
 
-                        Console.WriteLine("提醒邮件已成功发送！");
+                        // 将颜色更改为黄色并打印这一行
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} FMD提醒邮件已成功发送！");
+                        Console.ForegroundColor = originalColor;
                     }
                 }
             }
@@ -219,5 +212,6 @@ namespace AutoEmailSender
                 Console.WriteLine($"发送提醒邮件时出错: {ex.Message}");
             }
         }
+
     }
 }
